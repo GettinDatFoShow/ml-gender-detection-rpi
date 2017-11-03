c1ca76a941cc0bd8625cc6f743509b66fef962d4
gettindatfoshow <robert.dale.morris@gmail.com>
2017-11-02 20:11:58 -0400

    updated face_detect.py

--- scripts/face_detect.py
+++ scripts/face_detect.py
@@ -3,38 +3,65 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+# allow the camera to warmup
+time.sleep(0.1)
+
+# cap = cv2.VideoCapture(1)
 
 # Create the haar cascade
-faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+# faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 while(True):
 	# Capture frame-by-frame
-	ret, frame = cap.read()
 
-	# Our operations on the frame come here
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	# capture frames from the camera
+	for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+		# grab the raw NumPy array representing the image, then initialize the timestamp
+		# and occupied/unoccupied text
+		image = frame.array
 
-	# Detect faces in the image
-	faces = faceCascade.detectMultiScale(
-		gray,
-		scaleFactor=1.1,
-		minNeighbors=5,
-		minSize=(30, 30)
-		#flags = cv2.CV_HAAR_SCALE_IMAGE
-	)
+		# show the frame
+		cv2.imshow("Frame", image)
+		key = cv2.waitKey(1) & 0xFF
 
-	print("Found {0} faces!".format(len(faces)))
+		# clear the stream in preparation for the next frame
+		rawCapture.truncate(0)
 
-	# Draw a rectangle around the faces
-	for (x, y, w, h) in faces:
-		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+		# if the `q` key was pressed, break from the loop
+		if key == ord("q"):
+			break
+
+		# ret, frame = cap.read()
+
+		# Our operations on the frame come here
+		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+		# Detect faces in the image
+		faces = faceCascade.detectMultiScale(
+			gray,
+			scaleFactor=1.1,
+			minNeighbors=5,
+			minSize=(30, 30)
+			#flags = cv2.CV_HAAR_SCALE_IMAGE
+		)
+
+		print("Found {0} faces!".format(len(faces)))
+
+		# Draw a rectangle around the faces
+		for (x, y, w, h) in faces:
+			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
 
-	# Display the resulting frame
-	cv2.imshow('frame', frame)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+		# Display the resulting frame
+		cv2.imshow('frame', image)
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
 
 # When everything done, release the capture
 cap.release()
