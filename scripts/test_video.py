4c4aa82d07f4bd419951acdddf71c49ad9424b9f
gettindatfoshow <robert.dale.morris@gmail.com>
2017-11-08 11:05:29 -0500

    add haarcascad_frontalface_alt.xml file for cv face detection and updated test_video.py

--- scripts/test_video.py
+++ scripts/test_video.py
@@ -1,31 +1,37 @@
-# import the necessary packages
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import time
+import io
+import picamera
 import cv2
+import numpy
 
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
+# Create a memory stream so photos doesn't need to be saved in a file
+stream = io.BytesIO()
 
-# allow the camera to warmup
-time.sleep(0.1)
+# Get the picture (low resolution, so it should be quite fast)
+# Here you can also specify other parameters (e.g.:rotate the image)
+with picamera.PiCamera() as camera:
+    camera.resolution = (320, 240)
+    camera.capture(stream, format='jpeg')
 
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    # grab the raw NumPy array representing the image, then initialize the timestamp
-    # and occupied/unoccupied text
-    image = frame.array
+# Convert the picture into a numpy array
+buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
 
-    # show the frame
-    cv2.imshow("Frame", image)
-    key = cv2.waitKey(1) & 0xFF
+# Now creates an OpenCV image
+image = cv2.imdecode(buff, 1)
 
-    # clear the stream in preparation for the next frame
-    rawCapture.truncate(0)
+# Load a cascade file for detecting faces
+face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_alt.xml')
 
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
-        break
+# Convert to grayscale
+gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+# Look for faces in the image using the loaded cascade file
+faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+
+print("Found "+str(len(faces))+" face(s)")
+
+# Draw a rectangle around every found face
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0), 2)
+
+# Save the result image
+cv2.imwrite('result.jpg', image)
