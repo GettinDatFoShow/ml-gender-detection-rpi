c3046dedd9df8704c734c8ead33fc570a1207095
gettindatfoshow <robert.dale.morris@gmail.com>
2017-12-05 18:10:03 -0500

    fixed opencv problem, would not work if face was not detected

--- scripts/Img_detect_face.py
+++ scripts/Img_detect_face.py
@@ -1,62 +1,32 @@
-# #OpenCV module
-# import cv2
-# #os module for reading training data directories and paths
-# import matplotlib
-# #numpy to convert python lists to numpy arrause the face inside ys as it is needed by OpenCV face recognizers
-# import numpy
-#
-#
-# from matplotlib import pyplot as plt
-#
-# face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
-#
-# img = cv2.imread('../trial3.jpg')
-# # cv2.imshow('img', img)
-# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-# #Draw a rectangle around every found face
-# for (x,y,w,h) in faces:
-#     img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
-#     roi_gray = gray[y:y+h, x:x+w]
-#     roi_color = img[y:y+h, x:x+w]
-#     cv2.imshow('img', img)
-#     cv2.imshow('face', roi_color)
-#     cv2.imwrite('../tf_files/testing/found_face.jpg', roi_color)
-#
-# if(len(faces) < 0 ):
-#        print("no faces found")
-# print(faces)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-# import the necessary packages
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import time
+#OpenCV module
 import cv2
+#os module for reading training data directories and paths
+import matplotlib
+#numpy to convert python lists to numpy arrause the face inside ys as it is needed by OpenCV face recognizers
+import numpy
 
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
 
-# allow the camera to warmup
-time.sleep(0.1)
+from matplotlib import pyplot as plt
 
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    # grab the raw NumPy array representing the image, then initialize the timestamp
-    # and occupied/unoccupied text
-    image = frame.array
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 
-    # show the frame
-    cv2.imshow("Frame", image)
-    key = cv2.waitKey(1) & 0xFF
+img = cv2.imread('../trial3.jpg')
+# cv2.imshow('img', img)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+#Draw a rectangle around every found face
+for (x,y,w,h) in faces:
+    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    cv2.imshow('img', img)
+    cv2.imshow('face', roi_color)
+    cv2.imwrite('../tf_files/testing/found_face.jpg', roi_color)
 
-    # clear the stream in preparation for the next frame
-    rawCapture.truncate(0)
+if(len(faces) < 0 ):
+       print("no faces found")
+print(faces)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
-        break
+
--- scripts/test_video.py
+++ scripts/test_video.py
@@ -3,35 +3,34 @@ import picamera
 import cv2
 import numpy
 
-# Create a memory stream so photos doesn't need to be saved in a file
-stream = io.BytesIO()
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
 
-# Get the picture (low resolution, so it should be quite fast)
-# Here you can also specify other parameters (e.g.:rotate the image)
-with picamera.PiCamera() as camera:
-    camera.resolution = (320, 240)
-    camera.capture(stream, format='jpeg')
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
 
-# Convert the picture into a numpy array
-buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
+# allow the camera to warmup
+time.sleep(0.1)
 
-# Now creates an OpenCV image
-image = cv2.imdecode(buff, 1)
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    image = frame.array
 
-# Load a cascade file for detecting faces
-face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_alt.xml')
+    # show the frame
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
 
-# Convert to grayscale
-gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
 
-# Look for faces in the image using the loaded cascade file
-faces = face_cascade.detectMultiScale(gray, 1.1, 5)
-
-print("Found "+str(len(faces))+" face(s)")
-
-# Draw a rectangle around every found face
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0), 2)
-
-# Save the result image
-cv2.imwrite('result.jpg', image)
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
