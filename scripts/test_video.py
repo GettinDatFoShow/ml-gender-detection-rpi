5e9ca4f21ee4779f46a3b14bc963212bfd74ea47
gettindatfoshow <robert.dale.morris@gmail.com>
2017-12-05 18:32:05 -0500

    fixed opencv problem, would not work if face was not detected

--- scripts/test_video.py
+++ scripts/test_video.py
@@ -27,6 +27,20 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
     # show the frame
     cv2.imshow("Frame", image)
+    # Load a cascade file for detecting faces
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Look for faces in the image using the loaded cascade file
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+
+    print("Found " + str(len(faces)) + " face(s)")
+
+    # Draw a rectangle around every found face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 0), 2)
     key = cv2.waitKey(1) & 0xFF
 
     # clear the stream in preparation for the next frame
