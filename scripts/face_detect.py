44ab91e86c0a90a294082e5f2407689948caab91
gettindatfoshow <robert.dale.morris@gmail.com>
2017-12-07 13:15:09 -0500

    face gender model created face_detect.py

--- scripts/face_detect.py
+++ scripts/face_detect.py
@@ -12,9 +12,9 @@ import sys
 
 # import the necessary packages
 # from picamera.array import PiRGBArray
+from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
-import cv2
 import tensorflow as tf
 
 
