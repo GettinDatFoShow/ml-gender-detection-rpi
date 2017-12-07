e4ebc690c4e545e5700c2b3bd74028f313953872
gettindatfoshow <robert.dale.morris@gmail.com>
2017-12-07 13:17:46 -0500

    face gender model created face_detect.py

--- scripts/face_detect.py
+++ scripts/face_detect.py
@@ -66,8 +66,8 @@ def load_labels(label_file):
 
 if __name__ == "__main__":
     file_name = "./face_output.jpg"
-    model_file = "tf_files/retrained_graph.pb"
-    label_file = "tf_files/retrained_labels.txt"
+    model_file = "../tf_files/retrained_graph.pb"
+    label_file = "../tf_files/retrained_labels.txt"
     input_height = 224
     input_width = 224
     input_mean = 128
